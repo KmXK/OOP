77ad6fbfc12ec3f60584ad22f64970fac782221e
KmXK <guydo.k@yandex.by>
2022-03-04 20:44:13 +0300

    Edit files

--- Lab1_2/Figures.Drawers.WPF/FiguresWpfDrawer.cs
+++ Lab1_2/Figures.Drawers.WPF/FiguresWpfDrawer.cs
@@ -6,8 +6,8 @@ namespace Figures.Drawers.WPF;
 
 public abstract class FiguresWpfDrawer
 {
-    public Canvas Canvas { get; set; }
-    public Brush Stroke { get; set; }
-    public Brush Fill { get; set; }
+    public Canvas Canvas { get; init; }
+    public Brush Stroke { get; init; }
+    public Brush Fill { get; init; }
     public abstract Shape Draw(Figure figure);
 }
--- Lab1_2/Figures.Drawers/IFigureDrawer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Figures.Drawers
-{
-    public interface IFigureDrawer
-    {
-        void Draw(Figure figure);
-    }
-}
